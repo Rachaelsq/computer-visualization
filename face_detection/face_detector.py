da7a54f208aa52fbb0c631be596f8e8ce9a6fc1d
Rachaelsq <rachaelqsimone@gmail.com>
2021-12-24 20:31:21 -0600

    Update face_detector.py
    resized imagea

--- face_detection/face_detector.py
+++ face_detection/face_detector.py
@@ -19,18 +19,24 @@ my_file = os.path.join(this_folder, "photo.jpg")
 
 #read the img as grayscalle (increased accuracy, but colored img at the end)
 img = cv2.imread(my_file)
-
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 #find the xml file in the img and return the coordinates of where it exists(the face) in the img
 faces=face_cascade.detectMultiScale(gray_img,scaleFactor=1.05,minNeighbors=5)
 
+#draw the rectangle of coordinates onto the image
+for x, y, w, h in faces:
+    img=cv2.rectangle(img, (x,y),(x+w,y+h),(0, 255,0),3)
+    
+cv2.imshow("Gray", img)
 print(type(faces))
 print(faces)
 
+#resizing
+resized=cv2.resize(img,(int(img.shape[1]/3),int(img.shape[0]/3)))
 
 #execute
-cv2.imshow("Gray", gray_img)
+''' cv2.imshow("Gray", gray_img) '''
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
