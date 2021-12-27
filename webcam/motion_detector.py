3e225d0ba490d81959054c203f77841bf714af64
Rachaelsq <rachaelqsimone@gmail.com>
2021-12-27 11:14:44 -0600

    displaying start and end times
    used Bokeh to display start and end times of motion detection

--- webcam/motion_detector.py
+++ webcam/motion_detector.py
@@ -50,6 +50,8 @@ while True:
         (x, y, w, h)=cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
     status_list.append(status)
+    
+    # ======================status_list=status_list[2:]
     #recording the time of status changes
     if status_list[-1]==1 and status_list[-2]==0:
         times.append(datetime.now())
